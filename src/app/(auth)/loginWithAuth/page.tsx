4307e399dbd1e03f6f583492b9719f8596330f95
NISHTHA KHALASI <nishthakhalasi1@outlook.com>
2025-10-02 12:12:47 +0530

    first commit

--- src/app/(auth)/loginWithAuth/page.tsx
+++ src/app/(auth)/loginWithAuth/page.tsx
@@ -13,8 +13,8 @@ const montserrat = Montserrat({ subsets: ["latin"], weight: ["400", "500"] });
 
 export default function OtpPage() {
   return (
-    <div className="flex items-center justify-center h-screen-[90vh] ">
-      <Card className=" absolute top-[250px] w-full max-w-[537px] h-auto p-5 gap-2 border-[#BFBFBF] rounded-[10px] bg-white shadow-bottom-left sm:p-6 md:p-8">
+    <div className="flex items-center justify-center min-h-[90vh] px-4 ">
+      <Card className="w-full max-w-[537px] h-auto p-5 gap-6 border border-[#BFBFBF] rounded-[10px] bg-white shadow-bottom-left sm:p-6 md:p-8">
         <CardHeader className="items-center justify-center mt-[20px] mb-[20px]">
           <CardTitle
             className={`${montserrat.className} font-medium text-[32px] leading-[100%]  tracking-[0%] text-center`}
